88be41c78aaadebc1fd96848e04e71cd9e7735fb
kshahzeb1994 <kshahzeb1994@gmail.com>
2018-08-27 23:22:59 -0700

    initialized scores to 0

--- app.js
+++ app.js
@@ -17,6 +17,11 @@ var currentPlayer = 0;
 
 //hide
 document.querySelector('.dice').style.display = 'none';
+//set all scores to 0
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
     var dice = Math.floor(Math.random() * 6) + 1;
