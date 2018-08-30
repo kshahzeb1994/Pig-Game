01eb18b1a94fe374c284ed365134a238ee98c01e
kshahzeb1994 <kshahzeb1994@gmail.com>
2018-08-29 22:44:04 -0700

    Added: player toggle on 1

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
+// alert();
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
     var dice = Math.floor(Math.random() * 6) + 1;
@@ -29,8 +30,29 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
     diceDOM.src = "dice-" + dice + ".png";
-    // update score
 
+    // update score
+    if(dice !== 1){
+      	roundScore += dice;
+        document.getElementById('current-' + currentPlayer).textContent = roundScore;
+    } else {
+      // scores[currentPlayer] = 0;
+      roundScore = 0;
+
+      //reset scores
+      document.getElementById('score-' + currentPlayer).textContent = '0';
+      document.getElementById('current-' + currentPlayer).textContent = '0';
+
+      //update currentPlayer
+      currentPlayer = (currentPlayer + 1) % 2;
+
+      //change active class to next player
+      document.querySelector('.player-0-panel').classList.toggle('active');
+      document.querySelector('.player-1-panel').classList.toggle('active');
+
+      //hide dice again
+      document.querySelector('.dice').style.display = 'none';
+  }
 
 });
 
